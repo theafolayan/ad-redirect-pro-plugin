79cf7fe7f5d985eee7b851c771c7d758f1855948
theafolayan <theafolayan@gmail.com>
2021-08-09 07:41:31 +0100

    added hidden attribute to inputs

--- ad-redirect-pro.js
+++ ad-redirect-pro.js
@@ -3,17 +3,35 @@
       el: document.querySelector('#mount'),
       mounted: function(){
           const url = document.getElementById('kofem-media-url').value;
+          const seconds = parseInt(document.getElementById('kofem-media-seconds').value);
+          const nextUrl = document.getElementById('kofem-media-next-url').value;
           const userAgent = window.navigator.userAgent;
           if(userAgent.includes('FacebookBot')){
             console.log('Na Facebook Bot o!');
-        }else{
+        }else if(localStorage.getItem('kofem-visited' == 'true')){
+          localStorage.removeItem('kofem-visited');
+          window.location.href = nextUrl;
+        }
+        else{
             setTimeout( function(){
                 localStorage.setItem('kofem-visited', 'true');
-                window.location = url;
-            }, 10000);
-
+                window.location.href = url;
+            }, seconds);
         }
-        
       }
     });
   })();
+var nextUrl = document.getElementById('kofem-media-next-url').value;
+  (function(window, location) {
+    history.replaceState(null, document.title, location.pathname+"#!/history");
+    history.pushState(null, document.title, location.pathname);
+    
+    window.addEventListener("popstate", function() {
+      if(location.hash === "#!/history") {
+        history.replaceState(null, document.title, location.pathname);
+        setTimeout(function(){
+          location.replace(nextUrl);
+        },10);
+      }
+    }, false);
+    }(window, location)); 
